d353f24cfb0cd8ad472406faa217a46799bc5b8f
Ayman Salah <ayman.1551.salah@gmail.com>
2022-04-12 16:45:10 +0200

    Fixed a Typo in admin file

--- accounts/admin.py
+++ accounts/admin.py
@@ -3,24 +3,26 @@ from django.contrib.auth.admin import UserAdmin
 from .forms import CustomUserCreationForm, CustomUserChangeForm
 from .models import CustomUser
 # Register your models here.
+
+
 class CustomUserAdmin(UserAdmin):
     # Form To add a New User
     add_form = CustomUserCreationForm
-    
-
     # Form To Edit A User
     form = CustomUserChangeForm
-    
-
-    # Model To Edit/instaciate From 
+    # Model To Edit/instaciate From
     model = CustomUser
-
-
     # Fields To Show In The Admin Page
-    list_display = ['email', 'username', 'mobileNumber', 'city', 'is_staff']
+    list_display = ['email', 'username',
+                    'mobileNumber', 'is_merchant', 'is_staff', 'city']
 
     # Fields to be used in editing users
-    filedsets = UserAdmin.fieldsets + (None , {'fields': ('mobileNumber',)})
+    fieldsets = UserAdmin.fieldsets + \
+        ((None, {'fields': ('is_merchant', 'city',)}),)
+
+    add_fieldsets = UserAdmin.add_fieldsets + \
+        (None, {'fields': ('is_merchant', 'city',)}),
+
 
 # Fields To Show In The Admin Page
 admin.site.register(CustomUser, CustomUserAdmin)
